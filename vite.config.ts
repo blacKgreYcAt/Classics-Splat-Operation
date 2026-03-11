94d784994f231e995c9b5dcacd72ec9b7ec47f0c
blacKgreYcAt <ksbd7zs2yj@privaterelay.appleid.com>
2026-03-11 16:56:01 +0800

    Update Code

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Classics-Splat-Operation/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
